f60608d9c90e379c2510ceecd1bed0f6aa36b180
CarlosMario-Pro <cmario.reyesp@gmail.com>
2024-05-12 08:35:28 -0500

    Se incluyen los services

---
+++ services/User/accountsPayable.schema.ts
@@ -0,0 +1,192 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '../../db';
+import AccountsBook from './accountsBook.schema';
+import Assets from './assets.schema';
+import Merchandise from './merchandise.schema';
+import Product from './product.schema';
+import RawMaterial from './rawMaterial.schema';
+import Service from './service.schema';
+import Branch from './branch.schema';
+import User from './user.schema';
+
+class AccountsPayable extends Model {
+    public id!: string;
+    public transactionDate!: Date;
+    public transactionCounterpartId!: string;
+    public creditDescription!: string;
+    public stateAccount!: 'Activo' | 'Pagado';
+    public creditWithInterest!: 'Si' | 'No';
+    public creditInterestRate!: string;
+    public initialValue!: number;
+    public initialNumberOfPayments!: number;
+    public paymentValue!: number;
+    public currentBalance!: number;
+    public pendingNumberOfPayments!: number;
+    public creditPayments!: { date: Date; value: number; branchId: string; seller: string}[];
+    public cancellationDate!: Date;    
+    public seller!: string;
+    
+    //RELACION CON OTRAS TABLAS
+    public accountsBookId!: string;
+    public assetId!: string;
+    public merchandiseId!: string;
+    public productId!: string;
+    public rawMaterialId!: string;
+    public serviceId!: string;
+    public branchId!: string;
+    public userId!: string;
+};
+
+AccountsPayable.init(
+    {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false,
+            primaryKey: true,
+        },
+        transactionDate: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        transactionCounterpartId: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        creditDescription: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        stateAccount: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                isIn: [[ 'Activo', 'Pagado' ]],
+            },
+        },
+        creditWithInterest: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                isIn: [[ 'Si', 'No' ]],
+            },
+        },
+        creditInterestRate: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        initialValue: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        initialNumberOfPayments: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        paymentValue: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        currentBalance: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        pendingNumberOfPayments: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        creditPayments: {
+            type: DataTypes.JSON,
+            allowNull: true,
+            defaultValue: [],
+        },
+        cancellationDate: {
+            type: DataTypes.DATE,
+            allowNull: true,
+        },
+        seller: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        
+        //RELACION CON OTRAS TABLAS
+        accountsBookId: {
+            type: DataTypes.UUID,
+            allowNull: false,
+        },        
+        assetId: {
+            type: DataTypes.UUID,
+            allowNull: true,
+        },
+        merchandiseId: {
+            type: DataTypes.UUID,
+            allowNull: true,
+        },
+        productId: {
+            type: DataTypes.UUID,
+            allowNull: true,
+        },
+        rawMaterialId: {
+            type: DataTypes.UUID,
+            allowNull: true,
+        },
+        serviceId: {
+            type: DataTypes.UUID,
+            allowNull: true,
+        },
+        branchId: {
+            type: DataTypes.UUID,
+            allowNull: false,
+        },
+        userId: {
+            type: DataTypes.UUID,
+            allowNull: true,
+        },
+    },
+    {
+        sequelize: db,
+        modelName: 'AccountsPayable',
+    }
+);
+
+AccountsPayable.belongsTo(AccountsBook, {
+    foreignKey: 'accountsBookId',
+    as: 'accountsBook',
+});
+
+AccountsPayable.belongsTo(Assets, {
+    foreignKey: 'assetId',
+    as: 'assets',
+});
+
+AccountsPayable.belongsTo(Merchandise, {
+    foreignKey: 'merchandiseId',
+    as: 'merchandise',
+});
+
+AccountsPayable.belongsTo(Product, {
+    foreignKey: 'productId',
+    as: 'product',
+});
+
+AccountsPayable.belongsTo(RawMaterial, {
+    foreignKey: 'rawMaterialId',
+    as: 'rawMaterial',
+});
+
+AccountsPayable.belongsTo(Service, {
+    foreignKey: 'serviceId',
+    as: 'service',
+});
+
+AccountsPayable.belongsTo(Branch, {
+    foreignKey: 'branchId',
+    as: 'branch',
+});
+
+AccountsPayable.belongsTo(User, {
+    foreignKey: 'userId',
+    as: 'user',
+});
+
+export default AccountsPayable;
