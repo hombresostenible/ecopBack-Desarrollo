d5b598f0a3fe4f78b3de8008501f89c3fed6b92a
CarlosMario-Pro <cmario.reyesp@gmail.com>
2024-07-24 18:44:55 -0500

    Se refactorizan propiedades de ProductModel

--- types/User/products.types.ts
+++ types/User/products.types.ts
@@ -5,6 +5,18 @@ export interface IInventoryOffItem {
     description?: string;
 }
 
+export interface IWithholdingTax {
+    retentionType?: 'No tiene' | 'Retefuente' | 'Rete IVA' | 'Rete ICA';
+    retention?: 'retentionFeesConsulting' | 'retentionServices' | 'retentionPurchases' | 'retentionOthers' | 'retentionForeignPaymentsDividends';
+    retentionPercentageFeesConsulting?: '2' | '4' | '6' | '10' | '11';
+    retentionPercentageServices?: '1' | '2' | '3.5' | '4' | '6';
+    retentionPercentagePurchases?: '0.1' | '0.5' | '1' | '1.5' | '2.5' | '3' | '3.5';
+    retentionPercentageOthers?: '2' | '2.5' | '3' | '4' | '7' | '10' | '20';
+    retentionPercentageForeignPaymentsDividends?: '0' | '1' | '2' | '5' | '7' | '8' | '10' | '15' | '20' | '33' | '35' | '35 + Num. 51';
+    retentionPercentageIVA?: '15' | '100';
+    retentionPercentageICA?: '2' | '3.4' | '4.14' | '5' | '6.9' | '8' | '9.66' | '11.04' | '13.8';
+}
+
 export interface IProduct {
     id: string;
     barCode?: string;
@@ -47,21 +59,14 @@ export interface IProduct {
         quantity: string;
     }[];
     // Retenciones
-    retentionType?: 'No tiene' | 'Retefuente' | 'Rete IVA' | 'Rete ICA';
-    retentionPercentageFeesConsulting?: '2' | '4' | '6' | '10' | '11';
-    retentionPercentageServices?: '1' | '2' | '3.5' | '4' | '6';
-    retentionPercentagePurchases?: '0.1' | '0.5' | '1' | '1.5' | '2.5' | '3' | '3.5';
-    retentionPercentageOthers?: '2' | '2.5' | '3' | '4' | '7' | '10' | '20';
-    retentionPercentageForeignPaymentsDivideÑnds?: '0' | '1' | '2' | '5' | '7' | '8' | '10' | '15' | '20' | '33' | '35' | '35 + Num. 51';
-    retentionPercentageIVA?: '15' | '100';
-    retentionPercentageICA?: '2' | '3.4' | '4.14' | '5' | '6.9' | '8' | '9.66' | '11.04' | '13.8';
+    retentions: IWithholdingTax[];
     // Impuestos
     IVA: 0 | 5 | 19;
-    consumptionTax?: '4' | '8' | '16';
-    ivaAiu?: number;
+    consumptionTax?: 4 | 8 | 16;
+    ivaAiu?: 0 | 1;
     taxesUltraProcessedSugarSweetenedBeverages?: number;
-    valueTaxesUltraProcessedSugarSweetenedBeverages?: '0' | '18' | '28' | '35' | '38' | '55' | '65';
-    taxesUltraProcessedFoodProducts?: '10' | '15' | '20';
+    valueTaxesUltraProcessedSugarSweetenedBeverages?: 0 | 18 | 28 | 35 | 38 | 55 | 65;
+    taxesUltraProcessedFoodProducts?: 10 | 15 | 20;
     // IVA: number;
     
     //RELACION CON OTRAS TABLAS
