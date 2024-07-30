de803406b0b73c0f72564af0cf71e18a15ad5a30
TopDriveGroup <carlos.reyes@topdrivegroup.com>
2024-07-29 20:26:41 -0500

    Se agregan propiedades

--- data/User/allItems.data.ts
+++ data/User/allItems.data.ts
@@ -4,54 +4,40 @@ import sequelize from '../../db';
 //BUSCA TODOS LOS ARTICULOS DEL USUARIO EN TODAS LAS TABLAS
 export const getAllItemsData = async (userId: string): Promise<any> => {
     try {
-        const itemFound = await sequelize.query(`
-            SELECT 
-                id, branchId, barCode, nameItem, inventory, IVA, sellingPrice,
-                'Assets' as type, 
-                userId
-            FROM assets
-            WHERE userId = :userId
-
-            UNION ALL
-
-            SELECT 
-                id, branchId, barCode, nameItem, inventory, IVA, sellingPrice,
-                'Merchandise' as type, 
-                userId
-            FROM merchandises
-            WHERE userId = :userId
-
-            UNION ALL
-
-            SELECT
-                id, branchId, barCode, nameItem, inventory, IVA, sellingPrice,
-                'Product' as type, 
-                userId
-            FROM products
-            WHERE userId = :userId
-
-            UNION ALL
-
-            SELECT
-                id, branchId, barCode, nameItem, inventory, IVA, sellingPrice,
-                'RawMaterial' as type, 
-                userId
-            FROM rawMaterials
-            WHERE userId = :userId
-
-            UNION ALL
-
-            SELECT
-                id, branchId, barCode, nameItem, NULL as inventory, IVA, sellingPrice,
-                'Service' as type, 
-                userId
-            FROM services
-            WHERE userId = :userId
-        `, {
+        const assets = await sequelize.query('SELECT *, "Assets" as type FROM assets WHERE userId = :userId', {
             replacements: { userId },
-            type: QueryTypes.SELECT
+            type: QueryTypes.SELECT,
         });
-        return itemFound;
+
+        const merchandises = await sequelize.query('SELECT *, "Merchandise" as type FROM merchandises WHERE userId = :userId', {
+            replacements: { userId },
+            type: QueryTypes.SELECT,
+        });
+
+        const products = await sequelize.query('SELECT *, "Product" as type FROM products WHERE userId = :userId', {
+            replacements: { userId },
+            type: QueryTypes.SELECT,
+        });
+
+        const rawMaterials = await sequelize.query('SELECT *, "RawMaterial" as type FROM rawMaterials WHERE userId = :userId', {
+            replacements: { userId },
+            type: QueryTypes.SELECT,
+        });
+
+        const services = await sequelize.query('SELECT *, "Service" as type FROM services WHERE userId = :userId', {
+            replacements: { userId },
+            type: QueryTypes.SELECT,
+        });
+
+        const allItems = [
+            ...assets,
+            ...merchandises,
+            ...products,
+            ...rawMaterials,
+            ...services,
+        ];
+
+        return allItems;
     } catch (error) {
         throw error;
     }
