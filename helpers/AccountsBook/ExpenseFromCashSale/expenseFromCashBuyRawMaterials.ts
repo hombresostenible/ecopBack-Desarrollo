f1a5f7342a24c46ca02ac8353c89f358595df579
TopDriveGroup <carlos.reyes@topdrivegroup.com>
2024-09-06 17:49:04 -0500

    Ajustes de eliminacion de registros en el libro contable y tablas relacionadas

--- helpers/AccountsBook/ExpenseFromCashSale/expenseFromCashBuyRawMaterials.ts
+++ helpers/AccountsBook/ExpenseFromCashSale/expenseFromCashBuyRawMaterials.ts
@@ -4,17 +4,15 @@ import { ServiceError } from '../../../types/Responses/responses.types';
 
 export const expenseFromCashBuyRawMaterials = async (item: IItemsAccountsBook, branchId: string, transactionType: string): Promise<any> => {
     const rawMaterialFound = await RawMaterial.findOne({
-        where: { userId: item.id, nameItem: item.nameItem, branchId: branchId },
+        where: { id: item.id, nameItem: item.nameItem, branchId: branchId },
     });
     if (!rawMaterialFound) throw new ServiceError(400, "La materia prima no existe en esta sede");
     if (transactionType === 'Gasto') {
         if (item.quantity !== undefined) {
             try {
                 rawMaterialFound.inventory += item.quantity;
-
                 const currentDate = new Date().toISOString();
                 const quantity = item.quantity;
-
                 rawMaterialFound.setDataValue('inventoryChanges', rawMaterialFound.inventoryChanges.concat({ date: currentDate, quantity: quantity, type: 'Ingreso' }));
                 await rawMaterialFound.save();
             } catch (error) {
