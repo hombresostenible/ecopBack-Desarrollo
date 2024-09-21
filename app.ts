201cdcf0494329d392a57715d46a33b12ad362c3
Carlos Mario Reyes <cmario.reyesp@gmail.com>
2024-09-20 23:06:50 -0500

    a

--- app.ts
+++ app.ts
@@ -11,7 +11,6 @@ import { routerApi } from './controllers/routes';
 class Server {
     private app: Application;
     private port: string;
-    static listen: any;
 
     constructor() {
         this.app = express();
@@ -20,24 +19,23 @@ class Server {
         this.dbConnection();
         this.middlewares();
         this.routes();
-    };
+    }
 
     private async dbConnection() {
         try {
             await db.authenticate();
             console.log('Base de datos conectada');
             await db.sync();
-            // await db.sync({ force: true }); // En producción, puedes usar await db.sync(); en lugar de { force: true },Esto elimina y recrea las tablas en cada reinicio
             console.log('Modelos sincronizados con la base de datos');
         } catch (error) {
             console.error('Error al conectar la base de datos:', error);
             throw new Error('No se pudo conectar a la base de datos');
         }
-    };
+    }
 
     private routes() {
         routerApi(this.app);
-    };
+    }
 
     private middlewares() {
         // Middleware para verificar las cookies antes de CORS
@@ -46,22 +44,24 @@ class Server {
             next();
         });
 
-        // Se define así para permitir más de un dominio autorizado para enviar y recibir las respuestas del servidor
+        // Middleware para establecer la propiedad isSecure
+        this.app.use((req: Request, res: Response, next: NextFunction) => {
+            req.isSecure = req.headers['x-forwarded-proto'] === 'https' || req.protocol === 'https';
+            next();
+        });
+
+        // Configuración de CORS
         this.app.use(cors({
-            origin: process.env.CORS_ALLOWED_ORIGIN,                // Especifica el origen permitido
-            credentials: true,                                      // Habilita el intercambio de cookies entre el frontend y el backend
-            allowedHeaders: ['Content-Type', 'Authorization'],      // Especifica los encabezados permitidos
+            origin: process.env.CORS_ALLOWED_ORIGIN,
+            credentials: true,
+            allowedHeaders: ['Content-Type', 'Authorization'],
         }));
 
         this.app.use(express.json());
         this.app.use(morgan('dev'));
         this.app.use(cookieParser());
-
-        // Configurar bodyParser para aumentar el límite de carga
         this.app.use(bodyParser.json({ limit: '50mb' }));
         this.app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-
-        // Configurar express-fileupload
         this.app.use(fileUpload());
 
         // Middleware para procesar solicitudes XML
@@ -85,7 +85,7 @@ class Server {
                 next();
             }
         });
-    };
+    }
 
     public listen() {
         this.app.listen(this.port, () => {
@@ -94,7 +94,7 @@ class Server {
             console.error('Error al iniciar el servidor:', error);
             process.exit(1);
         });
-    };
-};
+    }
+}
 
 export default Server;
