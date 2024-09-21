6cf00a2611ab3b2204a5eca661a998e5ea937ab0
Carlos Mario Reyes <cmario.reyesp@gmail.com>
2024-09-20 22:41:33 -0500

    aaa

--- controllers/Auth/auth.controller.ts
+++ controllers/Auth/auth.controller.ts
@@ -23,10 +23,10 @@ router.post("/login", validateSchema(loginSchema), async (req: Request, res: Res
             const { serResult, token } = user.result;
             // res.cookie("token", token)
             res.cookie("token", token, {
-                httpOnly: false,
+                httpOnly: true,
                 secure: process.env.NODE_ENV === "production",
                 sameSite: "none",
-                // maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+                maxAge: 1000 * 60 * 60 * 24 * 1,
             });
             res.json({ serResult, token });
         } else res.status(user.code).json({ message: user.message });
