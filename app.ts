a27b75f448df3045c02f29728f7c0f466b08c597
Carlos Mario Reyes <104024599+CarlosMario-Pro@users.noreply.github.com>
2024-10-15 23:18:29 -0500

    Update app.ts

--- app.ts
+++ app.ts
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import fileUpload from 'express-fileupload';
 import morgan from 'morgan';
+import http from "http";
 import xml2js from 'xml2js';
 import db from './db';
 import { routerApi } from './controllers/routes';
@@ -11,11 +12,13 @@ import { routerApi } from './controllers/routes';
 class Server {
     private app: Application;
     private port: string;
+    private server: http.Server;
     static listen: any;
 
     constructor() {
         this.app = express();
         this.port = process.env.PORT || '8000';
+        this.server = http.createServer(this.app);
 
         this.dbConnection();
         this.middlewares();
@@ -27,7 +30,7 @@ class Server {
             await db.authenticate();
             console.log('Base de datos conectada');
             await db.sync();
-            // await db.sync({ force: true }); // En producción, puedes usar `await db.sync();` en lugar de `{ force: true }`,Esto elimina y recrea las tablas en cada reinicio
+            // await db.sync({ force: true });
             console.log('Modelos sincronizados con la base de datos');
         } catch (error) {
             console.error('Error al conectar la base de datos:', error);
@@ -40,28 +43,35 @@ class Server {
     };
 
     private middlewares() {
-        // Middleware para verificar las cookies antes de CORS
         this.app.use((req: Request, res: Response, next: NextFunction) => {
-            console.log('Cookies recibidas:', req.headers.cookie);
+            req.headers['x-forwarded-for'] || req.connection.remoteAddress;
             next();
         });
+        
+        this.app.set('trust proxy', true);
+        const allowedOrigins = [
+            process.env.CORS_ALLOWED_ORIGIN,
+            process.env.CORS_ALLOWED_ORIGIN2
+        ];
 
-        // Se define así para permitir más de un dominio autorizado para enviar y recibir las respuestas del servidor
         this.app.use(cors({
-            origin: 'https://platform.ecopcion.com',  // Especifica el origen permitido
-            credentials: true,                        // Permite el envío de cookies o credenciales
-            allowedHeaders: ['Content-Type', 'Authorization'], // Permite los headers requeridos
+            origin: (origin, callback) => {
+                console.log('Solicitud desde el origen:', origin);
+                if (origin && allowedOrigins.includes(origin)) {
+                    callback(null, true);
+                } else {
+                    callback(new Error('Not allowed by CORS'));
+                }
+            },
+            credentials: true,
+            allowedHeaders: ['Content-Type', 'Authorization'],
         }));
 
         this.app.use(express.json());
         this.app.use(morgan('dev'));
         this.app.use(cookieParser());
-
-        // Configurar bodyParser para aumentar el límite de carga
-        this.app.use(bodyParser.json({ limit: '50mb' }));
-        this.app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-
-        // Configurar express-fileupload
+        this.app.use(bodyParser.json({ limit: "50mb" }));
+        this.app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
         this.app.use(fileUpload());
 
         // Middleware para procesar solicitudes XML
@@ -85,17 +95,17 @@ class Server {
                 next();
             }
         });
-    };
+    }
 
     public listen() {
-        this.app.listen(this.port, () => {
-            console.log('SERVER ON PORT ' + this.port);
-        }).on('error', (error: NodeJS.ErrnoException) => {
-            console.error('Error al iniciar el servidor:', error);
+        this.server.listen(this.port, () => {
+            console.log(`Servidor escuchando en el puerto ${this.port}`);
+        }).on("Error", (error: NodeJS.ErrnoException) => {
+            console.error("Error al iniciar el servidor: ", error);
             process.exit(1);
         });
-    };
-};
+    }
+}
 
 export default Server;
 
